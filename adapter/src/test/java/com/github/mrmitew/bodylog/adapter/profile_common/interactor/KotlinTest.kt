cf947a11f1fa4d62ec7d8ecd1b24c8e67c95cb5d
Stefan Mitev <mr.mitew@gmail.com>
2017-09-27 14:19:46 +0200

    [add] Workaround to run Kotlin tests from Java modules

---
+++ adapter/src/test/java/com/github/mrmitew/bodylog/adapter/profile_common/interactor/KotlinTest.kt
@@ -0,0 +1,14 @@
+package com.github.mrmitew.bodylog.adapter.profile_common.interactor
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class KotlinTest {
+    @Test
+    fun testSomething() {
+        assertTrue("", true)
+    }
+}
